950c3a21deaf0e921f28f3f8daff38cc68149381
FabienMarti <martiretti.fabien@gmail.com>
2020-06-06 20:42:34 +0200

    e

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Part4</title>
+</head>
+<body>
+    <p>
+        <?php
+            function getMyString($myString){
+            echo($myString);
+            }
+            getMyString('WOW SALUT !!!');
+        ?>
+    </p>
+</body>
+</html>
